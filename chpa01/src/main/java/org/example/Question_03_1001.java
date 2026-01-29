897f5dde29b692549179272b3bdc4da721f61e39
이지윤 <>
2026-01-29 17:10:42 +0900

    백준 1-3까지 풀이

---
+++ chpa01/src/main/java/org/example/Question_03_1001.java
@@ -0,0 +1,13 @@
+package org.example;
+
+import java.util.Scanner;
+
+public class Question_03_1001 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+
+        System.out.print(a-b);
+    }
+}
