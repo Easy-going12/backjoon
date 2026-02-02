4153db51c3e543469ecadf05f4bf600f1e21a8bd
이지윤 <>
2026-02-02 16:39:51 +0900

    백준 1-5까지 풀이

---
+++ chpa01/src/main/java/org/example/Question_05_1008.java
@@ -0,0 +1,13 @@
+package org.example;
+
+import java.util.Scanner;
+
+public class Question_05_1008 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        double a = sc.nextDouble();
+        double b = sc.nextDouble();
+
+        System.out.print(a/b);
+    }
+}
