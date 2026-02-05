1b0e68dcd916dc934bfcecd72015b5a9bc08224e
이지윤 <>
2026-02-05 15:22:32 +0900

    백준 1-8까지 풀이

---
+++ chpa01/src/main/java/org/example/Question_08_18108.java
@@ -0,0 +1,11 @@
+package org.example;
+
+import java.util.Scanner;
+
+public class Question_08_18108 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        System.out.print(a - 543);
+    }
+}
