d082fd1f861cb52ae5e99f6f68c908e81cf7edd2
이지윤 <>
2026-02-06 16:09:07 +0900

    백준 1-9까지 풀이

---
+++ chpa01/src/main/java/org/example/Question_09_10430.java
@@ -0,0 +1,18 @@
+package org.example;
+
+import java.util.Scanner;
+
+public class Question_09_10430 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int A = sc.nextInt();
+        int B = sc.nextInt();
+        int C = sc.nextInt();
+
+        System.out.println((A+B)%C);
+        System.out.println(((A%C)+(B%C))%C);
+        System.out.println((A*B)%C);
+        System.out.print(((A%C)*(B%C))%C);
+    }
+}
