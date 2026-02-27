71b1f3eac29cc7ab7094b4291b7f08163527dd9a
이지윤 <>
2026-02-27 14:36:45 +0900

    백준 2-2까지 풀이

---
+++ BackJoon/src/main/java/chap02/Question_02_9498.java
@@ -0,0 +1,17 @@
+package chap02;
+
+import java.util.Scanner;
+
+public class Question_02_9498 {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+
+        int a = sc.nextInt();
+
+        if(a>=90) System.out.print("A");
+        else if(a<=89 && a>=80) System.out.print("B");
+        else if(a<=79 && a>=70) System.out.print("C");
+        else if(a<=69 && a>=60) System.out.print("D");
+        else System.out.print("F");
+    }
+}
