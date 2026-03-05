22fbcaebfad3b8f9d44df0686ed2ce09768b9dba
이지윤 <>
2026-03-05 12:16:07 +0900

    백준 2-3까지 풀이

---
+++ BackJoon/src/main/java/chap02/Question_03_2753.java
@@ -0,0 +1,14 @@
+package chap02;
+
+import java.util.Scanner;
+
+public class Question_03_2753 {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+
+        int a = sc.nextInt();
+
+        if(a%4 == 0 && (a%100 != 0 | a%400 == 0))System.out.print(1);
+        else System.out.print(0);
+    }
+}
